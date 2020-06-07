3855eb23b0346e5adac3905915f9f353102ecb1e
Luke <luke@cloudthrifty.com>
2020-06-07 16:08:19 -0400

    transfer update

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module junk
 go 1.13
 
 require (
-	github.com/goog-lukemc/gouidom v0.0.0-20200504130024-c92fcab294dd
-	github.com/goog-lukemc/gouielement v0.0.0-20200526012208-4a511d9ff222
-	github.com/goog-lukemc/tserver v0.0.0-20200512162719-aa4dffa3a601
+	github.com/goog-lukemc/gouidom v0.0.12
+
+	github.com/goog-lukemc/gouielement v0.0.27
+	github.com/goog-lukemc/gouiscreen v0.0.10
+
+	github.com/goog-lukemc/tserver v0.0.0-20200529164431-ab867c9e1c47
 )
--- main.go
+++ main.go
@@ -15,14 +15,21 @@ func main() {
 
 	go unload()
 
-	app, err := gouidom.NewApp("Wellness")
+	app, err := gouidom.NewApp("Reader")
 	if err != nil {
 		gouidom.CLog("%v", err)
 		unloadMsg <- 1 // Unloads WASM with a exit log
 	}
 
 	comp := gouielement.NewElementLib(app)
-	gouiscreen.Reader(nil, comp, "html/body")
+
+	gouiscreen.DocReader("html/body", comp, nil)
+
+	if app.GetCurrentPath() == "/app/edit" {
+		comp.CodeBlock("html/body", app.GetHTMLDocument())
+		comp.CodeBlock("html/body", app.GenStyleTemplate())
+		comp.CodeBlock("html/body", app.GetAppStyle())
+	}
 
 	<-done
 }
@@ -35,3 +42,25 @@ func unload() {
 		}
 	}
 }
+
+// Reader create a readable content
+// func Reader(content *gouielement.ReadingData, comp *gouielement.ElementLib, parent string) error {
+// 	if content == nil {
+// 		content = getDefaultContent(parent, comp)
+// 	}
+// 	comp.Readable("html/body", content)
+// 	return nil
+// }
+
+// func getDefaultContent(parent string, comp *gouielement.ElementLib) *gouielement.ReadingData {
+// 	c := &gouielement.ReadingData{
+// 		Title:    "Welcome to the Go UI framework using Webassembly",
+// 		Subtitle: "Calm down!, this project is a WIP",
+// 	}
+
+// 	t1 := "Welcome to the goui framework. Have fun!"
+// 	c.Content = append(c.Content, comp.IMG(parent, map[string]string{"src": "/image/go-logo_blue.svg", "width": "40", "height": "30"}))
+// 	c.Content = append(c.Content, comp.Span(parent, t1))
+
+// 	return c
+// }
